78e3beb0866166163ee7facf98d06c70e123bd4d
Dave Rios <yefriddavid@gmail.com>
2019-03-20 13:40:02 -0300

    polishing

--- app/app.js
+++ app/app.js
@@ -15,19 +15,19 @@ const redis = require('redis'),
 
 const PORT = 80
 
+
+// Routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/broadcast', (req, res) => {
 	socketServer.to('milkyway').emit('chatmessage', {message: "Hello everybody!", server: hostname})
-	// socketServer.broadcast('milkyway'.emit("holaaaaaaaaaa"))
   res.send(`Message sent to everybody! ${hostname}`)
 })
 
 app.get('/sendTo', (req, res) => {
 	socketServer.to(req.query.nickname).emit('chatmessage', {message: `Hello:  ${req.query.nickname}`, server: hostname})
-	// socketServer.broadcast('milkyway'.emit("holaaaaaaaaaa"))
   res.send(`Message sent to! ${req.query.nickname} throw: ${hostname}`)
 })
 
